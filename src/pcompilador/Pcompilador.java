e81fa0aa400e7ba94ac8bce3f2346c143c8a6437
iq231097 <hfelipec@miumg.edu.gt>
2018-05-21 15:39:17 -0600

    inicio de proyecto

--- src/pcompilador/Pcompilador.java
+++ src/pcompilador/Pcompilador.java
@@ -15,7 +15,7 @@ public class Pcompilador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola mundo");
     }
     
 }
